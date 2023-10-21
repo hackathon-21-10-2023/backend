39f5d95408aa78638b80d8396b31042372ac29e7
Kirill Kulikov <to+git@adoge.ru>
2023-10-21 18:35:51 +0300

    models

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User
-
+from .models import User, Department, Feedback, FeedbackItem
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Department, admin.ModelAdmin)
+admin.site.register(Feedback, admin.ModelAdmin)
+admin.site.register(FeedbackItem, admin.ModelAdmin)
--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 
@@ -13,7 +14,6 @@ class Department(models.Model):
 class User(AbstractUser):
     name = models.CharField(max_length=100, verbose_name='Имя')
     surname = models.CharField(max_length=100, verbose_name='Фамилия')
-    password = models.CharField(max_length=100, verbose_name='Пароль')
     email = models.EmailField(unique=True, verbose_name='Email')
     department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='users', verbose_name='Отдел', null=True, blank=True)
     position = models.CharField(max_length=100, verbose_name='Должность')
@@ -21,6 +21,7 @@ class User(AbstractUser):
     is_intern = models.BooleanField(default=False, verbose_name='Является стажером')
     is_head = models.BooleanField(default=False, verbose_name='Является руководителем')
     is_awaiting_feedback = models.BooleanField(default=False, verbose_name='Ожидает отзыва')
+    feedback_viewed = models.ForeignKey("Feedback", on_delete=models.CASCADE, related_name='feedback_viewed', verbose_name='Просмотренные отзывы', null=True, blank=True)
 
     def __str__(self):
         return f"{self.name} {self.surname} <{self.email}>"
@@ -28,3 +29,39 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+
+class Feedback(models.Model):
+    to_user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='feedback_to_user', verbose_name='Пользователь')
+    from_user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='feedback_from_user', verbose_name='Отправитель')
+    score = models.IntegerField(verbose_name='Оценка', validators=[MinValueValidator(1), MaxValueValidator(5)])
+    text = models.TextField(verbose_name='Текст')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    score_tone = models.IntegerField(verbose_name='Тональность оценки', validators=[MinValueValidator(-1), MaxValueValidator(1)])
+
+    class Meta:
+        verbose_name = 'Общий отзыв'
+        verbose_name_plural = 'Общие отзывы'
+
+    def __str__(self):
+        return f"{self.from_user} -> {self.to_user}"
+
+    def save(
+        self, force_insert=False, force_update=False, using=None, update_fields=None
+    ):
+        self.score_tone = 0
+        for item in self.feedback_items.all():
+            self.score_tone += item.score_tone
+        self.score_tone /= len(self.feedback_items.all())
+        super().save()
+
+
+class FeedbackItem(models.Model):
+    metric_name = models.CharField(max_length=500, verbose_name='Название метрики')
+    text = models.TextField(verbose_name='Текст')
+    score_tone = models.IntegerField(verbose_name='Тональность оценки', validators=[MinValueValidator(-1), MaxValueValidator(1)])
+    feedback = models.ForeignKey(Feedback, on_delete=models.CASCADE, related_name='feedback_items', verbose_name='Общий отзыв')
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
